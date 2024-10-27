9657f0dff913b7f0f3819c4d5472918906bbadc0
Nhoem Tevy <tevymomnh193@gmail.com>
2024-10-27 13:50:29 +0700

    PUSH BY TEVY

---
+++ lib/pages/profile_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+
+class profile_screen extends StatelessWidget {
+  const profile_screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,44 @@
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(SplashScreen());
+// }
+// class SplashScreen extends StatefulWidget {
+//   @override
+//   _SplashScreenState createState() => _SplashScreenState();
+// }
+//
+// class _SplashScreenState extends State<SplashScreen> {
+//   @override
+//   void initState() {
+//     super.initState();
+//
+//     // Navigate to HomeScreen after 3 seconds
+//     Future.delayed(Duration(seconds: 3), () {
+//       Navigator.pushReplacement(
+//         context,
+//         MaterialPageRoute(builder: (context) => HomeScreen()),
+//       );
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Color(0xFF0E1217), // Background color
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Image.asset('assets/images/daily.png'), // Your splash screen logo
+//             SizedBox(height: 20),
+//             CircularProgressIndicator(color: Colors.white), // Loading indicator
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+//
+// class HomeScreen {
+// }
