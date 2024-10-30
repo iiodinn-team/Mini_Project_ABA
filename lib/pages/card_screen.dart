873470409a6688a8a084ad8fe5a837d76cdd005b
Nhoem Tevy <tevymomnh193@gmail.com>
2024-10-30 23:16:36 +0700

    4 Page: Splash_screen, Card_Screen, Profile_screen, Payment_screen

---
+++ lib/pages/card_screen.dart
@@ -0,0 +1,203 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CardScreen());
+}
+
+class CardScreen extends StatelessWidget {
+  const CardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Card Screen",
+      debugShowCheckedModeBanner: false,
+      home: PamentHomePage(),
+    );
+  }
+}
+
+final List<Map<String, dynamic>> items = [
+  {
+    "image": Image.network(
+      'https://static.wikia.nocookie.net/mobile-legends/images/e/ee/Mobile_Legends_5v5_Logo_Fair_MOBA_for_Mobile.JPG/revision/latest?cb=20200819062413',
+      fit: BoxFit.cover, // Ensure the image fills the circle
+      width: 40.0, // Match the container size
+      height: 40.0,
+      errorBuilder: (context, error, stackTrace) {
+        return const Icon(Icons.error, color: Colors.red); // Fallback icon on error
+      },
+    ),
+    "title": "Mobile Legend: Bang Bang",
+    "subtitle": "Debit Card",
+  },
+  {
+    "image": Image.network(
+      'https://www.paisabazaar.com/wp-content/uploads/2022/10/Featured-Image-11-768x511.jpg',
+      fit: BoxFit.cover, // Ensure the image fills the circle
+      width: 40.0, // Match the container size
+      height: 40.0,
+      errorBuilder: (context, error, stackTrace) {
+        return const Icon(Icons.error, color: Colors.red); // Fallback icon on error
+      },
+    ),
+    "title": "Debit Card",
+    "subtitle": "order your ABA debit card easily and choose your receiving option  between delivery, pick up at branch or instantly at kiosk. ",
+  },
+  {
+    "image": Image.network(
+      'https://www.adcb.com/en/Images/1250x560-conventional_tcm41-528838.png',
+      fit: BoxFit.cover, // Ensure the image fills the circle
+      width: 40.0, // Match the container size
+      height: 40.0,
+      errorBuilder: (context, error, stackTrace) {
+        return const Icon(Icons.error, color: Colors.red); // Fallback icon on error
+      },
+    ),
+    "title": "Virtual Debit Card",
+    "subtitle": "Create virtual Visa, Mastercard or UnionPay International card instantly for free and start making payments in stores and online.",
+  },
+  // Add more items as needed
+];
+
+class PamentHomePage extends StatelessWidget {
+  const PamentHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF002E44),
+        leading: const Icon(Icons.arrow_back, color: Colors.white, size: 32),
+        titleSpacing: 0,
+        title: const Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'ABA',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.w600,
+                fontFamily: 'KantumruyPro',
+              ),
+            ),
+            SizedBox(width: 10),
+            Text(
+              'Cards',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                fontFamily: 'KantumruyPro',
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+              radius: 50,
+              backgroundColor: Colors.white24,
+              child: Icon(
+                Icons.local_atm_rounded,
+                size: 60,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'New Card',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'KantumruyPro',
+              ),
+            ),
+            const SizedBox(height: 32),
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                color: const Color(0xFF091015),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Special Edition Debit Cards',
+                        style: TextStyle(color: Colors.white70, fontSize: 22, fontFamily: 'KantumruyPro'),
+                      ),
+                      const SizedBox(height: 10),
+                      Expanded(
+                        child: ListView.builder(
+                          itemCount: items.length,
+                          itemBuilder: (context, index) {
+                            final item = items[index];
+                            return Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: Card(
+                                color: const Color(0xFF101D26),
+                                margin: const EdgeInsets.symmetric(vertical: 4.0),
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(vertical: 6.0),
+                                  child: ListTile(
+                                    leading: item["image"] != null
+                                        ? Container(
+                                      width: 40.0,
+                                      height: 40.0,
+                                      decoration: const BoxDecoration(
+                                        color: Colors.white24,
+                                        shape: BoxShape.circle,
+                                      ),
+                                      child: ClipOval(
+                                        child: SizedBox(
+                                          width: 40.0,
+                                          height: 40.0,
+                                          child: item["image"],
+                                        ),
+                                      ),
+                                    )
+                                        : Container(
+                                      width: 40.0,
+                                      height: 40.0,
+                                      decoration: const BoxDecoration(
+                                        color: Colors.white24,
+                                        shape: BoxShape.circle,
+                                      ),
+                                    ),
+                                    title: Text(
+                                      item["title"],
+                                      style: const TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                        fontFamily: 'KantumruyPro',
+                                      ),
+                                    ),
+                                    subtitle: Text(
+                                      item["subtitle"],
+                                      style: const TextStyle(color: Colors.white70, fontSize: 14, fontFamily: 'KantumruyPro'),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: const Color(0xFF002E44),
+    );
+  }
+}
--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,44 +1,131 @@
-// import 'package:flutter/material.dart';
-//
-// void main() {
-//   runApp(SplashScreen());
-// }
-// class SplashScreen extends StatefulWidget {
-//   @override
-//   _SplashScreenState createState() => _SplashScreenState();
-// }
-//
-// class _SplashScreenState extends State<SplashScreen> {
-//   @override
-//   void initState() {
-//     super.initState();
-//
-//     // Navigate to HomeScreen after 3 seconds
-//     Future.delayed(Duration(seconds: 3), () {
-//       Navigator.pushReplacement(
-//         context,
-//         MaterialPageRoute(builder: (context) => HomeScreen()),
-//       );
-//     });
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Color(0xFF0E1217), // Background color
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             Image.asset('assets/images/daily.png'), // Your splash screen logo
-//             SizedBox(height: 20),
-//             CircularProgressIndicator(color: Colors.white), // Loading indicator
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-//
-// class HomeScreen {
-// }
+import 'package:flutter/material.dart';
+import 'package:mini_project_aba/pages/profile_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: SplashScreen(),
+  ));
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Navigate to ProfileScreen after 3 seconds
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => ProfileScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0xFF002E44), // Background color
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text.rich(
+              TextSpan(
+                children: [
+                  TextSpan(
+                    text: 'ABA', // First part of the text
+                    style: TextStyle(
+                      fontSize: 28, // Example font size
+                      fontWeight: FontWeight.bold, // Example font weight
+                      color: Colors.white, // Example text color
+                    ),
+                  ),
+                  TextSpan(
+                    text: '\' ', // First part of the text
+                    style: TextStyle(
+                      fontSize: 28, // Example font size
+                      fontWeight: FontWeight.bold, // Example font weight
+                      color: Colors.red, // Example text color
+                    ),
+                  ),
+                  TextSpan(
+                    text: 'Mobile', // Second part of the text with different style
+                    style: TextStyle(
+                      fontSize: 28, // Custom font size
+                      fontWeight: FontWeight.bold, // Custom font weight
+                      color: Colors.white, // Custom text color
+                    ),
+                  ),
+                ],
+              ),
+              textAlign: TextAlign.center,
+            // ),
+            // Column(
+            // children: [
+            // const SizedBox(height: 16), // Add some space before the button
+            // TextButton(
+            // onPressed: () {
+            // // Navigate to ProfileScreen immediately when the button is pressed
+            // Navigator.pushReplacement(
+            // context,
+            // MaterialPageRoute(builder: (context) => const ProfileScreen()),
+            // );
+            // },
+            // child: const Text.rich(
+            // TextSpan(
+            // children: [
+            // TextSpan(
+            // text: 'ABA', // First part of the text
+            // style: TextStyle(
+            // fontSize: 24, // Example font size
+            // fontWeight: FontWeight.bold, // Example font weight
+            // color: Colors.white, // Example text color
+            // ),
+            // ),
+            // TextSpan(
+            // text: '\' ', // Second part of the text
+            // style: TextStyle(
+            // fontSize: 24, // Example font size
+            // fontWeight: FontWeight.bold, // Example font weight
+            // color: Colors.red, // Example text color
+            // ),
+            // ),
+            // TextSpan(
+            // text: 'Mobile', // Third part of the text with different style
+            // style: TextStyle(
+            // fontSize: 24, // Custom font size
+            // fontWeight: FontWeight.bold, // Custom font weight
+            // color: Colors.white, // Custom text color
+            // ),
+            // ),
+            // ],
+            // ),
+            // ),
+            // ),
+            // ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Screen'),
+      ),
+      body: Center(
+        child: Text('Welcome to the Home Screen!'),
+      ),
+    );
+  }
+}
